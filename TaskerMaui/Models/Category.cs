4c5e77b9f762959bf52a2fe652c43d9f0235eb80
Luis Ramirez <luisf350@gmail.com>
2022-09-06 10:16:59 -0500

    Add new Style and Horizontal CollectionView

---
+++ TaskerMaui/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskerMaui.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int PendingTask { get; set; }
+        public float Percentage { get; set; }
+    }
+}
--- TaskerMaui/ViewModels/MainViewModel.cs
+++ TaskerMaui/ViewModels/MainViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using TaskerMaui.Models;
 
 namespace TaskerMaui.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        public ObservableCollection<Category> Categories { get; set; }
+
+        public MainViewModel()
+        {
+            FillData();
+        }
+
+        private void FillData()
+        {
+            Categories = new ObservableCollection<Category>
+            {
+                new Category{ Id=1, Name=".NET MAUI Course", Color="#CF14DF"},
+                new Category{ Id=2, Name="Tutorials", Color="#df6f14"},
+                new Category{ Id=3, Name="Shopping", Color="#14df80"}
+            };
+        }
     }
 }
